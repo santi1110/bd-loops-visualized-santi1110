24b687adbda69418f67ef21dc7ef4554aa7d99de
Santiago Coria <santiago_coria@hotmail.com>
2024-02-10 22:59:02 -0300

    Loops

--- src/processing/sketches/Main.java
+++ src/processing/sketches/Main.java
@@ -26,7 +26,7 @@ public class Main extends PApplet {
     public void draw() {
         background(360);
 
-        switch(paperMode) {
+        switch (paperMode) {
             case LINED:
                 drawLinedPaper(80, 100, 30);
                 break;
@@ -46,20 +46,72 @@ public class Main extends PApplet {
 
     public void drawLinedPaper(int leftMargin, int topMargin, int gapHeight) {
         // TODO: Finish this method.
+        stroke(0, 30, 100);
+        line(leftMargin, 0, leftMargin, height);
+
+        stroke(240, 30, 100);
+        int lineNumber = 0;
+        while (topMargin + lineNumber * gapHeight < height) {
+            line(0, topMargin + lineNumber * gapHeight, width, topMargin + lineNumber * gapHeight);
+            lineNumber++;
+        }
     }
 
     public void drawGridPaper(int boxLength, boolean isEngineer) {
         // TODO: Finish this method.
+        //vertical stroke
+        stroke(0, 30, 100);
+
+        int lineNumber = 0;
+        for (int x = boxLength; x < width; x += boxLength){
+            if (lineNumber % 5 == 1) {
+                strokeWeight(3);
+            }else{
+                strokeWeight(1);
+            }
+            line(x, 0, x, height);
+            lineNumber++;
+        }
+        //horizontal stroke
+        stroke(210, 30, 100);
+        lineNumber = 0;
+        for (int y = boxLength; y < height; y += boxLength){
+            if (lineNumber % 5 == 1) {
+                strokeWeight(3);
+            }else{
+                strokeWeight(1);
+            }
+            line(0, y, width, y);
+            lineNumber++;
+        }
     }
 
     public void drawDotPaper(int gap, int radius) {
         // TODO: Finish this method.
+        stroke(230,30,100);
+
+        for (int x = 0; x < width; x+= gap) {
+            for (int y = 0; y < height; y += gap) {
+                ellipse(x,y,2*radius, 2*radius);
+            }
+
+        }
+
+
+
+
+
+
     }
 
     public void drawPrettyPaper() {
+        for (int  x = 0; x < width; x++){
+            for (int y = 0; y < height; y ++){
+                int chosenColor = color((float) x/ width *360,(float) y/ height *100, 100);
+                set(x,y,chosenColor);
+        }
         // TODO: Finish this method.
-    }
-
+    }}
 
 
     // Below here should be left alone. //
